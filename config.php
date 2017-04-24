dc3a7d0af62edc1e3f186ef254ab611ba3b306a4
forkballpitch <forkballpitch@gmail.com>
2017-04-25 00:15:45 +0900

    Add files via upload
    github 정상작동 x 로 인해 수동 커밋

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Pusher chat
+ * facebook like chat jQuery plugin using Pusher API 
+ * version: 1.0   
+ * Author & support : zied.hosni.mail@gmail.com 
+ * © 2012 html5-ninja.com
+ * for more info please visit http://html5-ninja.com
+ * 
+ */
+
+// create an account on http://pusher.com/ to obtain API access
+$key = 'bed10b5b2a28263682b8';
+$secret = 'a67672f14fc538a09364';
+$app_id = '265302';
+?>
